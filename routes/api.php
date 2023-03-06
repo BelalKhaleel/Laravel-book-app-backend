ba62d3f66a8ac51e15263a9fc1798f18a5763a48
fadi <fadi@codi.tech>
2023-03-06 14:40:01 +0200

    JWT Authentication Implementation

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,3 +28,14 @@ Route::Post('/category',[BookController::class,'addCategory']);
 Route::Post('/book',[BookController::class,'addBook']);
 Route::Get('/book/{id}',[BookController::class,'getBook']);
 Route::Patch('/book/{id}',[BookController::class,'editBook']);
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/admin-profile', [AuthController::class, 'adminProfile']);    
+});
